f16ee038266ba508d42d9fa83593e76b0927f9a6
Denis <47882549+sncodeGit@users.noreply.github.com>
2019-09-18 22:32:31 +0300

    Add const MAX_UNIQUE.

--- Preprocessing/get_range_of_column.py
+++ Preprocessing/get_range_of_column.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Sep 12 03:32:57 2019
-Поиск диапазона значений столбцов.
-Максимум количества выводимых неодинаковых записей - 100.
+Максимум количества выводимых неодинаковых записей - MAX_UNIQUE.
 Если неодинаковых записей больше, то прога об этом уведомит.
 @author: inter000
 """
@@ -10,7 +9,8 @@ Created on Thu Sep 12 03:32:57 2019
 import pymssql
 import pandas as pd
 
-length = 892489 # Количество записей в выгрузке
+LENGTH = 892489 # Количество записей в выгрузке
+MAX_UNIQUE = 100 # Максимальное количество выводимых неодинаковых записей. Выведет предупреждение при превышении.
 
 df = pd.read_excel('top1.xlsx') # top1.xlsx - файл с названиями столбцов (см. Google диск)
 ser = pd.Series(df.columns) # Можно использовать для целочисленного перебора по номерам столбцов 
@@ -21,12 +21,12 @@ conn = pymssql.connect(host='localhost', user='sa',
 cur = conn.cursor()
 
 for i in ('IdCaseType1',): # Список столбцов или столбец. ТАкже может быть целочисленным диапазоном, но
-    req = 'SELECT ' + i + ' FROM dbo.Запрос2' # Тогда необходимо заменить i на ser[i] здесь и ввыводе.
+    req = 'SELECT ' + i + ' FROM dbo.Запрос2' # Тогда необходимо заменить i на ser[i] здесь и в выводе.
     cur.execute(req)
     ans = cur.fetchall()
     
     data_range = []
-    for j in range(length):
+    for j in range(LENGTH):
         if (ans[j][0] is not None):
             flag = 0 # Совпадает ли с предыдущими значениями столбца
             for k in range(len(data_range)):
@@ -35,8 +35,8 @@ for i in ('IdCaseType1',): # Список столбцов или столбец
                     break
             if (flag == 0):
                 data_range.append(ans[j][0])
-            if (len(data_range) > 100):
-                print('Next more than 100!')
+            if (len(data_range) > MAX_UNIQUE):
+                print('Next more than ', MAX_UNIQUE, '!')
                 break
         
     print(i, ': ', sorted(data_range))
